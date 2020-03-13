b1727558a6fa614c3fac1328f498013b3f633d51
el-hackerino <el-hackerino@users.noreply.github.com>
2020-03-13 09:24:56 +0100

    Constants

--- hoard-calc.js
+++ hoard-calc.js
@@ -1,117 +1,87 @@
-// TODO constants?
-const TROOPS = [
-  { shortName: "C", name: "Coin Purse", percent: 5, xp: 10 },
-  { shortName: "R", name: "Gold Ring", percent: 10, xp: 25 },
-  { shortName: "P", name: "Priest's Chalice", percent: 20, xp: 50 },
-  { shortName: "K", name: "King's Crown", percent: 25, xp: 100 },
-  { shortName: "G", name: "Genie' Lamp", percent: 30, xp: 250 },
-  { shortName: "S", name: "Sacred Treasure", percent: 50, xp: 500 }
-];
-const TEMPLATES = [
-  [2, 2, 2, 2, 2], // 100, 0
-  [3, 2, 2, 2, 2], // 105, 1
-  [3, 3, 2, 2, 1], // 100, 2
-  [3, 3, 3, 2, 1], // 105, 3
-  [3, 3, 3, 2, 2], // 115, 4
-  [3, 3, 3, 3],   // 100, 5
-  [4, 2, 2, 2, 1], // 100, 6
-  [4, 3, 2, 2, 1], // 105, 7
-  [4, 3, 3, 1, 1], // 100, 8
-  [4, 3, 3, 2],   // 100, 9
-  [4, 3, 3, 3],   // 105, 10
-  [4, 4, 2, 1, 1], // 100, 11
-  [4, 4, 2, 2],   // 100, 12
-  [5, 2, 1, 1, 1], // 100, 13
-  [5, 2, 2, 1],   // 100, 14
-  [4, 4, 3, 1, 1], // 105, 15
-  [4, 4, 3, 2],   // 105, 16
-  [5, 3, 1, 1, 1], // 105, 17
-  [4, 4, 3, 3],   // 110, 18
-  [5, 3, 3],     // 100, 19
-  [4, 4, 4, 1],   // 100, 20
-  [5, 4, 1, 1],   // 100, 21
-  [4, 4, 4, 2],   // 110, 22
-  [5, 4, 2],     // 100, 23
-  [4, 4, 4, 3],   // 115, 24
-  [5, 4, 3],     // 105, 25
-  [4, 4, 4, 4],   // 120, 26
-  [5, 4, 4],     // 110, 27
-  [5, 5],       // 100, 28
-];
-const TEMPLATES_QUICK = [0, 1, 3, 4, 5, 9, 10, 12, 14, 19, 20, 23, 25, 26, 27, 28];
-
 const RUN_ITERATIONS = 1000;
 const RANDOMIZE = 0;
 
-var AMOUNTS = [0, 5, 2, 18, 11, 4];
-var INITIAL_QUALITY = 1;
-var INITIAL_LEVEL = 6;
-var INITIAL_XP = 0; // leftover
+var AMOUNTS = [0, 5, 28, 28, 1, 6];
+//var AMOUNTS = [0, 5, 2, 18, 11, 4];
+var INITIAL_QUALITY = 6;
+var INITIAL_LEVEL = 63;
+var INITIAL_XP = 5; // leftover
 var GOAL_LEVEL = 100;
 var GOAL_QUALITY = 10;
 
-start();
+const INPUTS = [
+  document.querySelector('#t1'),
+  document.querySelector('#t2'),
+  document.querySelector('#t3'),
+  document.querySelector('#t4'),
+  document.querySelector('#t5'),
+  document.querySelector('#t6'),
+];
 
-function start() {
-  console.log("Starting...");
-  if (window.Worker) {
-    let solutions = [];
-    let totalComboCounts = [];
-    let maxTroopCounts = [];
-    let totalTime = 0;
-    let totalSlowTime = 0;
-    const myWorker = new Worker("worker.js");
-    let solution = {
-      iterations: RANDOMIZE ? RUN_ITERATIONS : 1,
-      settings: {
-        randomize: RANDOMIZE,
-        budget: AMOUNTS,
-        initialQuality: INITIAL_QUALITY,
-        initialLevel: INITIAL_LEVEL,
-        initialXp: INITIAL_XP,
-        goalLevel: GOAL_LEVEL,
-        goalQuality: GOAL_QUALITY
+if (window.Worker) {
+  let solutions = [];
+  let totalComboCounts = [];
+  let maxTroopCounts = [];
+  let totalTime = 0;
+  let totalSlowTime = 0;
+  const myWorker = new Worker("worker.js");
+  for (let input of INPUTS) {
+    input.onchange = function () {
+      console.log("Calculating...");
+      for (let [i, input] of INPUTS.entries()) {
+        AMOUNTS[i] = input.value;
       }
-    };
-    if (RANDOMIZE) {
-      myWorker.postMessage(solution);
-    } else {
-      solution.settings.useQuickList = true;
-      myWorker.postMessage(solution);
-      solution.settings.useQuickList = false;
-      myWorker.postMessage(solution);
-    }
-
-    myWorker.onmessage = function(e) {
-      let solution = e.data;
+      let solution = {
+        iterations: RANDOMIZE ? RUN_ITERATIONS : 1,
+        settings: {
+          randomize: RANDOMIZE,
+          budget: AMOUNTS,
+          initialQuality: INITIAL_QUALITY,
+          initialLevel: INITIAL_LEVEL,
+          initialXp: INITIAL_XP,
+          goalLevel: GOAL_LEVEL,
+          goalQuality: GOAL_QUALITY
+        }
+      };
       if (RANDOMIZE) {
-        solutions.push(solution);
-        // Count used combos
-        let comboCounts = solution.slowSolution.comboCounts;
-        for (let c = 0; c < comboCounts.length; c++) {
-          if (comboCounts[c]) {
-              totalComboCounts[c] = totalComboCounts[c] ? totalComboCounts[c] + comboCounts[c] : comboCounts[c];
-          }
-        }
-        let solutionTroopCounts = solution.troopCounts;
-        // Save max troop counts
-        for (let t = 0; t < solution.troopCounts.length; t++) {
-          if (solution.troopCounts[t]) {
-              maxTroopCounts[t] = maxTroopCounts[t] ? Math.max(maxTroopCounts[t], solution.troopCounts[t]) : solution.troopCounts[t];
-          }
-        }
-        totalTime += solution.time;
-        totalSlowTime += solution.slowTime;
-        renderTests(solutions, totalComboCounts, totalTime / solutions.length, totalSlowTime / solutions.length);
-        console.log("Max troop counts: " + maxTroopCounts);
+        myWorker.postMessage(solution);
       } else {
-        renderSolution(solution);
-        console.log("Time: " + (solution.time / 1000) + " s");
+        solution.settings.useQuickList = 1;
+        myWorker.postMessage(solution);
+        // solution.settings.useQuickList = false;
+        // myWorker.postMessage(solution);
       }
     }
-  } else { // TODO
-    console.log('Your browser doesn\'t support web workers.')
   }
+
+  myWorker.onmessage = function (e) {
+    let solution = e.data;
+    if (RANDOMIZE) {
+      solutions.push(solution);
+      // Count used combos
+      let comboCounts = solution.slowSolution.comboCounts;
+      for (let c = 0; c < comboCounts.length; c++) {
+        if (comboCounts[c]) {
+          totalComboCounts[c] = totalComboCounts[c] ? totalComboCounts[c] + comboCounts[c] : comboCounts[c];
+        }
+      }
+      // Save max troop counts
+      for (let t = 0; t < solution.troopCounts.length; t++) {
+        if (solution.troopCounts[t]) {
+          maxTroopCounts[t] = maxTroopCounts[t] ? Math.max(maxTroopCounts[t], solution.troopCounts[t]) : solution.troopCounts[t];
+        }
+      }
+      totalTime += solution.time;
+      totalSlowTime += solution.slowTime;
+      renderTests(solutions, totalComboCounts, totalTime / solutions.length, totalSlowTime / solutions.length);
+      console.log("Max troop counts: " + maxTroopCounts);
+    } else {
+      renderSolution(solution);
+      console.log("Time: " + (solution.time / 1000) + " s");
+    }
+  }
+} else { // TODO
+  console.log('Your browser doesn\'t support web workers.')
 }
 
 function renderTests(solutions, totalComboCounts, avgTime, avgslowTime) {
@@ -120,7 +90,7 @@ function renderTests(solutions, totalComboCounts, avgTime, avgslowTime) {
 
   const comboTable = createTable(["Combo", "Troops", "Freq", "Slow"]);
   comboTable.id = 'combo-table';
-	comboTable.classList.add('mainTable');
+  comboTable.classList.add('mainTable');
   document.body.appendChild(comboTable);
 
   for (let [key, value] of Object.entries(totalComboCounts).filter(([key, value]) => value > 0)) {
@@ -142,18 +112,18 @@ function renderTests(solutions, totalComboCounts, avgTime, avgslowTime) {
   td.colSpan = 4;
   td.textContent = solutions.length + " iterations, avg time: " + parseInt(avgTime) + " ms, avg slow time: " + parseInt(avgslowTime);
 
-	const table = createTable(["Budget", "Gold", "Level", "Quality", "Time", "Slow", "Slow G", "Combos", "Slow Combos", "In Level", "In Quality"]);
-	table.id = 'main-table';
-	table.classList.add('mainTable');
+  const table = createTable(["Budget", "Gold", "Level", "Quality", "Time", "Slow", "Slow G", "Combos", "Slow Combos", "In Level", "In Quality"]);
+  table.id = 'main-table';
+  table.classList.add('mainTable');
   document.body.appendChild(table);
   // TODO sorttable.makeSortable(table);
 
   for (let solution of solutions) {
     if (!solution) continue;
     let tr = table.insertRow(-1);
-		for (let attribute of ['budget', 'bestCost', 'bestLevel', 'bestQuality', 'time', 'slowTime', 'quickCostDiff', 'combos', 'slowCombos', 'initialLevel', 'initialQuality']) {
-			let td = tr.insertCell(-1);
-			if (attribute == 'budget') {
+    for (let attribute of ['budget', 'bestCost', 'bestLevel', 'bestQuality', 'time', 'slowTime', 'quickCostDiff', 'combos', 'slowCombos', 'initialLevel', 'initialQuality']) {
+      let td = tr.insertCell(-1);
+      if (attribute == 'budget') {
         td.textContent = '';
         for (let troopNr of solution.budget) {
           td.textContent += troopNr += ", ";
@@ -177,54 +147,58 @@ function renderTests(solutions, totalComboCounts, avgTime, avgslowTime) {
       } else {
         td.textContent = solution[attribute];
       }
-		}
+    }
   }
 }
 
 function renderSolution(solution) {
-	const table = createTable(["Troops", "%", "XP", "Cost", "Level", "Quality", "Extra XP"]);
-	table.id = 'main-table';
-	table.classList.add('mainTable');
+  removeElement('main-table');
+  const table = createTable(["Troops", "%", "XP", "Cost", "Level", "Quality", "Extra XP"]);
+  table.id = 'main-table';
+  table.classList.add('mainTable');
   document.body.appendChild(table);
   //sorttable.makeSortable(table);
 
   for (let step of solution.bestSteps) {
     let tr = table.insertRow(-1);
-		for (let attribute of ['troops', 'percent', 'xp', 'cost', 'level', 'quality', 'extraXp']) {
-			let td = tr.insertCell(-1);
-			if (attribute == 'troops') {
+    for (let attribute of ['troops', 'percent', 'xp', 'cost', 'level', 'quality', 'extraXp']) {
+      let td = tr.insertCell(-1);
+      if (attribute == 'troops') {
         td.textContent = '';
         for (let troop of step.troops) {
           td.textContent += TROOPS[troop].shortName;
         }
-			} else {
+      } else {
         td.textContent = step[attribute];
       }
-		}
+    }
   }
 
   let tr = table.insertRow(-1);
   let td = tr.insertCell(-1);
   td.textContent = solution.bestCost;
+  tr = table.insertRow(-1);
+  td = tr.insertCell(-1);
+  td.textContent = solution.iterations;
 }
 
 function createTable(COLUMN_NAMES) {
   let table = document.createElement('table');
-	// Headers
-	let tr = table.insertRow(-1);
-	COLUMN_NAMES.forEach(function(columnName, i) {
-		let th = document.createElement('th');
-		th.textContent = columnName;
-		tr.appendChild(th);
+  // Headers
+  let tr = table.insertRow(-1);
+  COLUMN_NAMES.forEach(function (columnName, i) {
+    let th = document.createElement('th');
+    th.textContent = columnName;
+    tr.appendChild(th);
   });
   return table;
 }
 
 function removeElement(id) {
-	if (document.getElementById(id)) {
-		const oldElement = document.getElementById(id);
-		oldElement.parentNode.removeChild(oldElement);
-	}
+  if (document.getElementById(id)) {
+    const oldElement = document.getElementById(id);
+    oldElement.parentNode.removeChild(oldElement);
+  }
 }
 
 function reset() {
